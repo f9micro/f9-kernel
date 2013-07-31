21a0a10a8be67543a097bf35798ae477b944c243
arcb <arcbbb@gmail.com>
2013-07-31 10:07:10 +0800

    reorganize handler priority

--- platform/irq.c
+++ platform/irq.c
@@ -10,8 +10,17 @@ void irq_init()
 	/* Set all 4-bit to pre-emption priority bit */
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 
-	/* Set default priority */
-	NVIC_SetPriority(SysTick_IRQn, 0x1, 0);
-	NVIC_SetPriority(SVCall_IRQn, 0x1, 0);
+	/* Set default priority from high to low */
+
+	NVIC_SetPriority(DebugMonitor_IRQn, 0x0, 0);
+
+	NVIC_SetPriority(MemoryManagement_IRQn, 0x1, 0);
+	NVIC_SetPriority(BusFault_IRQn, 0x1, 0);
+	NVIC_SetPriority(UsageFault_IRQn, 0x1, 0);
+
+	/* Priority 0x2 - debug_uart */
+
+	NVIC_SetPriority(SysTick_IRQn, 0x2, 0);
+	NVIC_SetPriority(SVCall_IRQn, 0x2, 0);
 }
 
