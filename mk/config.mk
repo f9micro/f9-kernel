0eed2ce7db58777ad3d6d6766f42174a6f1698a2
Jim Huang <jserv@0xlab.org>
2013-08-01 05:49:04 +0800

    mk: mention the limitation of configurations

--- mk/config.mk
+++ mk/config.mk
@@ -2,6 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# FIXME: the current pattern can not handle the following definition:
+#	#define CONFIG_KTIMER_HEARTBEAT		65536
+# It will be set to "y" forcely, which is incorrect.
+#
 config_list := $(shell $(CC) -E -dM include/config.h | \
 	grep -o "\#define CONFIG_[A-Za-z0-9_]*" | cut -c9- | sort)
 $(foreach c,$(config_list) $(config_list),$(eval $(c)=y))
