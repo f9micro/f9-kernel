b574103f7cf20ad9b06f80dc06121222f76b77f4
GeorgeKang <georgekang03@gmail.com>
2013-07-31 17:58:29 +0800

    Support nested IRQ and increase IRQ performance by doing context switch at necessary condition.

--- include/platform/irq.h
+++ include/platform/irq.h
@@ -10,12 +10,7 @@
 #include <thread.h>
 #include <sched.h>
 #include <platform/link.h>
-/*
- * TODO: current implementation is highly ineffective because:
- * 1. It is does not support Nested Interrupts. At present, interrupt nesting
- *    will rewrite irq_stack_pointer which is global)
- * 2. We always save all registers even if we do not perform context switching.
- */
+#include <platform/cortex_m.h>
 
 void irq_init();
 
@@ -49,18 +44,19 @@ static inline int irq_number()
 /*
  * irq_save()
  *
- * Saves {r4-r11}, msp, psp, EXC_RETURN
+ * Saves {r4-r11}, msp, psp
  */
 #define irq_save(ctx) \
-	__asm__ __volatile__ ("mov r0, %0" : : "r" ((ctx)->regs));	\
+	__asm__ __volatile__ ("cpsid i");			\
+	__asm__ __volatile__ ("mov r0, %0" : : "r" ((ctx)->regs) : "r0");	\
 	__asm__ __volatile__ ("stm r0, {r4-r11}");			\
 	__asm__ __volatile__ ("cmp lr, #0xFFFFFFF9");			\
 	__asm__ __volatile__ ("ite eq");				\
-	__asm__ __volatile__ ("mrseq r0, msp");				\
-	__asm__ __volatile__ ("mrsne r0, psp");				\
+	__asm__ __volatile__ ("mrseq r0, msp"::: "r0");				\
+	__asm__ __volatile__ ("mrsne r0, psp"::: "r0");				\
 	__asm__ __volatile__ ("mov %0, r0" : "=r" ((ctx)->sp) : );
 
-#define irq_return(ctx) \
+#define irq_restore(ctx) \
 	__asm__ __volatile__ ("mov lr, %0" : : "r" ((ctx)->ret));	\
 	__asm__ __volatile__ ("mov r0, %0" : : "r" ((ctx)->sp));	\
 	__asm__ __volatile__ ("mov r2, %0" : : "r" ((ctx)->ctl));	\
@@ -71,7 +67,7 @@ static inline int irq_number()
 	__asm__ __volatile__ ("mov r0, %0" : : "r" ((ctx)->regs));	\
 	__asm__ __volatile__ ("ldm r0, {r4-r11}");			\
 	__asm__ __volatile__ ("msr control, r2");			\
-	__asm__ __volatile__ ("bx lr");
+	__asm__ __volatile__ ("cpsie i");			
 
 /* Initial context switches to kernel.
  * It simulates interrupt to save corect context on stack.
@@ -85,23 +81,51 @@ static inline int irq_number()
 	__asm__ __volatile__ ("cpsie i");				\
 	__asm__ __volatile__ ("bx r1");
 
+#define irq_enter()									\
+		__asm__ __volatile__ ("push {lr}");			
+
+#define irq_return()									\
+		__asm__ __volatile__ ("pop {lr}");			\
+		__asm__ __volatile__ ("bx lr");
+
+#define context_switch(from, to)					\
+{												\
+	 	__asm__ __volatile__ ("pop {lr}");		\
+		irq_save(&(from)->ctx);					\
+		thread_switch((to));					\
+		irq_restore(&(from)->ctx);				\
+		__asm__ __volatile__ ("bx lr");			\
+}
+
+#define schedule_in_irq()						\
+{												\
+	register tcb_t *sel;						\
+	sel = schedule_select();					\
+	if(sel != current)							\
+		context_switch(current, sel);			\
+}
+
+#define NO_PREEMPTED_IRQ						\
+	(*SCB_ICSR & SCB_ICSR_RETTOBASE)
+
 /*
- * Context is switched on interrupt return
- * We check if nobody schedules actions in kernel (SOFTIRQs)
- * Then do context switch
+ * Context is switched when the selected thead is different
+ * to current then do context switch.
  *
  * Idea is that on interrupt we'll save all registers under
  * irq_stack_pointer than on return we copy registers to
  * thread's structure or to kernel_ctx
  * */
+
 #define IRQ_HANDLER(name, sub) \
-	void name() __NAKED;			\
-	void name()				\
-	{					\
-		irq_save(&current->ctx);	\
-		sub();				\
-		schedule();			\
-		irq_return(&current->ctx);	\
+	void name() __NAKED;							\
+	void name()										\
+	{												\
+		irq_enter();								\
+		sub();										\
+		if(NO_PREEMPTED_IRQ)						\
+			schedule_in_irq();						\
+		irq_return();								\
 	}
 
 extern volatile tcb_t *current;
