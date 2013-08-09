f321695a8ed6573679f79bfe751668034f900f0b
arcb <arcbbb@gmail.com>
2013-08-09 19:47:45 +0800

    Makefile: improve parsing for include/config.h
    Fixes #34

--- mk/config.mk
+++ mk/config.mk
@@ -2,10 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# FIXME: the current pattern can not handle the following definition:
-#	#define CONFIG_KTIMER_HEARTBEAT		65536
-# It will be set to "y" forcely, which is incorrect.
-#
 config_list := $(shell $(CC) -E -dM include/config.h | \
-	grep -o "\#define CONFIG_[A-Za-z0-9_]*" | cut -c9- | sort)
+	sed -n 's/^\#define \(CONFIG_[^ \t]\+\) $$/\1/p' | sort)
 $(foreach c,$(config_list) $(config_list),$(eval $(c)=y))
