435ea0133acef2a84fe00835b2abd43c1c749869
southernbear <xq5530303@gmail.com>
2013-08-27 07:21:44 +0800

    systick: reload need to be one less than heartbeat

--- platform/stm32f4/systick.c
+++ platform/stm32f4/systick.c
@@ -9,12 +9,12 @@
 void init_systick(uint32_t tick_reload, uint32_t tick_next_reload)
 {
 	/* 250us at 168Mhz */
-	*SYSTICK_RELOAD = tick_reload;
+	*SYSTICK_RELOAD = tick_reload - 1;
 	*SYSTICK_VAL = 0;
 	*SYSTICK_CTL = 0x00000007;
 
 	if (tick_next_reload)
-		*SYSTICK_RELOAD = tick_next_reload;
+		*SYSTICK_RELOAD = tick_next_reload - 1;
 }
 
 void systick_disable()
