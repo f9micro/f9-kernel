0f4705a9a12748e0caaa1827c88294895102dcbd
southernbear <xq5530303@gmail.com>
2013-09-10 09:54:09 +0800

    add l4test to all-y

---
+++ user/apps/build.mk
@@ -0,0 +1,7 @@
+# Copyright (c) 2013 The F9 Microkernel Project. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+app-dir = user/apps
+
+include $(app-dir)/l4test/build.mk
---
+++ user/apps/l4test/build.mk
@@ -0,0 +1,8 @@
+# Copyright (c) 2013 The F9 Microkernel Project. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+user-dirs += user/apps/l4test
+
+user-apps-y = \
+	main.o
