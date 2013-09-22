0a11bedf0bf7c52d8e6424ab299ac1f42e640507
Viller Hsiao <villerhsiao@gmail.com>
2013-09-23 07:59:42 +0800

    mk: remove obsolete config variables
    The config variables are generated automatically by including .config
    
    Signed-off-by: Viller Hsiao <villerhsiao@gmail.com>

--- mk/config.mk
+++ mk/config.mk
@@ -2,12 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-AUTOCONF_H := include/autoconf.h
-config_list := $(shell [ -f $(AUTOCONF_H) ] || touch $(AUTOCONF_H) ; \
-	$(CC) $(CPPFLAGS) -E -dM $(AUTOCONF_H) | \
-	grep -o "\#define CONFIG_[A-Za-z0-9_]*" | cut -c9- | sort)
-$(foreach c,$(config_list) $(config_list),$(eval $(c)=y))
-
 MCONF_MSG = mainmenu "F9 Microkernel Configurations"
 define append_mconf_body
 	MCONF_MSG += \n$(1)
