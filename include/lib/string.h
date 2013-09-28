3526387509beee2021318866f7e27ecf1e96593f
Viller Hsiao <villerhsiao@gmail.com>
2013-09-28 12:15:45 +0800

    lib: string: add memcpy and memset
    Introduce string utilities from tag v0.9.14
    of MUSL libc project(http://www.musl-libc.org/) with the following
    modifications,
    
    1. Remove unused ports and functions
    2. Tweak for F9 Microkernel environment
    
    Signed-off-by: Viller Hsiao <villerhsiao@gmail.com>

---
+++ include/lib/string.h
@@ -0,0 +1,14 @@
+/* Copyright (c) 2013 The F9 Microkernel Project. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef	LIB_STRING_H_
+#define	LIB_STRING_H_
+
+#include <types.h>
+
+void *memcpy (void *__restrict, const void *__restrict, size_t);
+void *memset (void *, int, size_t);
+
+#endif /* LIB_STRING_H_ */
