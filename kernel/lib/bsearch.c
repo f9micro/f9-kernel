1fef4f805e48ea03015680aed0af40510402aab4
Jim Huang <jserv@0xlab.org>
2014-04-19 03:30:27 +0800

    lib: remove unnecessary asserts in bsearch

--- kernel/lib/bsearch.c
+++ kernel/lib/bsearch.c
@@ -33,16 +33,13 @@ void *bsearch(const void *key, const void *base0, size_t nmemb, size_t size,
 	int cmp;
 	const void *p;
 
-	assert(base0 != NULL || nmemb == 0);
-	assert(compar != NULL);
-
 	for (lim = nmemb; lim != 0; lim >>= 1) {
 		p = base + (lim >> 1) * size;
 		cmp = (*compar)(key, p);
 		if (cmp == 0)
-			return (void *)p;
+			return (void *) p;
 		if (cmp > 0) {	/* key > p: move right */
-			base = (const char *)p + size;
+			base = (const char *) p + size;
 			lim--;
 		}		/* else move left */
 	}
