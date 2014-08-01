85a6e2ea48ebd7157bd1bb58e5c98036ab9752de
Jim Huang <jserv@0xlab.org>
2014-08-01 11:44:33 +0800

    platform: clean up implementation annotation

--- platform/stm32f429/hwtimer.c
+++ platform/stm32f429/hwtimer.c
@@ -1,6 +1,3 @@
-/* Copyright (c) 2014 The F9 Microkernel Project. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
+/* no additional platform specific implementation */
 
 /* No Unshareable code temporary */
