f8ac6db249980e83cfaf161bf0dad9fdce379c40
Jim Huang <jserv@0xlab.org>
2014-08-26 23:11:05 +0800

    platform: stm32f1: simplify the annotation of no platform-specific

--- platform/stm32f1/systick.c
+++ platform/stm32f1/systick.c
@@ -1,5 +1,1 @@
-/* Copyright (c) 2014 The F9 Microkernel Project. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
+/* no additional platform specific implementation */
