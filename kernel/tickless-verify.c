fddf1ba04d8b3194d156cf21738669d43d7e1f5a
Jim Huang <jserv.tw@gmail.com>
2016-11-30 14:21:37 +0800

    tickless-verify: simplify naming scheme

--- kernel/tickless-verify.c
+++ kernel/tickless-verify.c
@@ -9,11 +9,11 @@
 #include <debug.h>
 #include <tickless-verify.h>
 
-static int tickless_verify_enabled;
-static int tickless_verify_started;
+static int verify_enabled;
+static int verify_started;
 
-static int tickless_verify_n = 0;
-static int tickless_verify_times = 0;
+static int verify_n = 0;
+static int verify_times = 0;
 
 static struct {
 	uint32_t ktimer_diff;
@@ -21,20 +21,20 @@ static struct {
 	uint32_t need;
 	uint32_t count;
 	uint32_t count_int;
-} tickless_verify_records[TICKLESS_VERIFY_MAX_RECORD];
+} verify_records[TICKLESS_VERIFY_MAX_RECORD];
 
-static uint32_t tickless_verify_start_need;
-static uint32_t tickless_verify_start_ktimer;
-static uint32_t tickless_verify_start_systick;
-static uint32_t tickless_verify_start_hwtimer;
+static uint32_t verify_start_need;
+static uint32_t verify_start_ktimer;
+static uint32_t verify_start_systick;
+static uint32_t verify_start_hwtimer;
 
 /* Count tickless entering times during start and stop */
-static uint32_t tickless_verify_start_count;
+static uint32_t verify_start_count;
 
 /* Count times of tickless interrupted by not systick interrupt */
-static uint32_t tickless_verify_start_count_int;
+static uint32_t verify_start_count_int;
 
-static void tickless_verify_save(uint32_t *systick, uint32_t *hwtimer)
+static void verify_save(uint32_t *systick, uint32_t *hwtimer)
 {
 	*systick = CONFIG_KTIMER_HEARTBEAT - systick_now();
 	*hwtimer = hwtimer_now();
@@ -42,37 +42,34 @@ static void tickless_verify_save(uint32_t *systick, uint32_t *hwtimer)
 
 void tickless_verify_init()
 {
-	int i;
-
-	if (tickless_verify_enabled == 0) {
+	if (verify_enabled == 0) {
 		hwtimer_init();
 
-		for (i = 0; i < TICKLESS_VERIFY_MAX_RECORD; i++) {
-			tickless_verify_records[i].need = 0;
-			tickless_verify_records[i].ktimer_diff = 0;
-			tickless_verify_records[i].hwtimer_diff = 0;
-			tickless_verify_records[i].count = 0;
-			tickless_verify_records[i].count_int = 0;
+		for (int i = 0; i < TICKLESS_VERIFY_MAX_RECORD; i++) {
+			verify_records[i].need = 0;
+			verify_records[i].ktimer_diff = 0;
+			verify_records[i].hwtimer_diff = 0;
+			verify_records[i].count = 0;
+			verify_records[i].count_int = 0;
 		}
 
-		tickless_verify_n = 0;
-		tickless_verify_times = 0;
+		verify_n = 0;
+		verify_times = 0;
 
-		tickless_verify_enabled++;
+		verify_enabled++;
 	}
 }
 
 void tickless_verify_start(uint32_t ktimer_now, uint32_t need)
 {
-	tickless_verify_save(&tickless_verify_start_systick,
-	                     &tickless_verify_start_hwtimer);
+	verify_save(&verify_start_systick, &verify_start_hwtimer);
 
-	tickless_verify_start_need = need;
-	tickless_verify_start_ktimer = ktimer_now;
-	tickless_verify_start_count = 0;
-	tickless_verify_start_count_int = 0;
+	verify_start_need = need;
+	verify_start_ktimer = ktimer_now;
+	verify_start_count = 0;
+	verify_start_count_int = 0;
 
-	tickless_verify_started = tickless_verify_enabled;
+	verify_started = verify_enabled;
 }
 
 void tickless_verify_stop(uint32_t ktimer_now)
@@ -82,44 +79,44 @@ void tickless_verify_stop(uint32_t ktimer_now)
 	uint32_t ktimer_diff;
 	uint32_t hwtimer_diff;
 
-	tickless_verify_save(&systick, &hwtimer);
+	verify_save(&systick, &hwtimer);
 
-	ktimer_diff = (ktimer_now - tickless_verify_start_ktimer) * CONFIG_KTIMER_HEARTBEAT
-	              + systick - tickless_verify_start_systick;
-	hwtimer_diff = (hwtimer - tickless_verify_start_hwtimer) * 2;
+	ktimer_diff = (ktimer_now - verify_start_ktimer) * CONFIG_KTIMER_HEARTBEAT
+	              + systick - verify_start_systick;
+	hwtimer_diff = (hwtimer - verify_start_hwtimer) * 2;
 
-	if (!tickless_verify_started)
+	if (!verify_started)
 		return;
 
-	if (tickless_verify_n >= TICKLESS_VERIFY_MAX_RECORD)
-		tickless_verify_n = 0;
+	if (verify_n >= TICKLESS_VERIFY_MAX_RECORD)
+		verify_n = 0;
 
-	tickless_verify_records[tickless_verify_n].need =
-	    tickless_verify_start_need * CONFIG_KTIMER_HEARTBEAT -
-	    tickless_verify_start_systick;
-	tickless_verify_records[tickless_verify_n].ktimer_diff = ktimer_diff;
-	tickless_verify_records[tickless_verify_n].hwtimer_diff = hwtimer_diff;
-	tickless_verify_records[tickless_verify_n].count = tickless_verify_start_count;
-	tickless_verify_records[tickless_verify_n].count_int = tickless_verify_start_count_int;
+	verify_records[verify_n].need =
+	    verify_start_need * CONFIG_KTIMER_HEARTBEAT -
+	    verify_start_systick;
+	verify_records[verify_n].ktimer_diff = ktimer_diff;
+	verify_records[verify_n].hwtimer_diff = hwtimer_diff;
+	verify_records[verify_n].count = verify_start_count;
+	verify_records[verify_n].count_int = verify_start_count_int;
 
-	tickless_verify_n++;
-	tickless_verify_times++;
+	verify_n++;
+	verify_times++;
 }
 
 void tickless_verify_count()
 {
-	tickless_verify_start_count++;
+	verify_start_count++;
 }
 
 void tickless_verify_count_int()
 {
-	tickless_verify_start_count_int++;
+	verify_start_count_int++;
 }
 
 int32_t tickless_verify_stat(int *times)
 {
 	int32_t sum = 0;
-	int i, n = tickless_verify_times;
+	int n = verify_times;
 	uint32_t compensation = UINT32_MAX;
 	uint32_t int_compensation = UINT32_MAX;
 
@@ -134,68 +131,70 @@ int32_t tickless_verify_stat(int *times)
 	           " REAL-KTIM", "COUNT(INT)");
 #endif
 
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		int32_t hwtimer_need_diff =
-		    tickless_verify_records[i].hwtimer_diff -
-		    tickless_verify_records[i].need;
+		    verify_records[i].hwtimer_diff -
+		    verify_records[i].need;
 #ifdef CONFIG_KDB
 		int32_t hwtimer_ktimer_diff =
-		    tickless_verify_records[i].hwtimer_diff -
-		    tickless_verify_records[i].ktimer_diff;
+		    verify_records[i].hwtimer_diff -
+		    verify_records[i].ktimer_diff;
 
 		dbg_printf(DL_KDB,
 		"%c Record %2d: %10d | %10d | %10d | %c%9d | %c%9d | %5d(%d)\n",
-		           i == tickless_verify_n - 1 ? '*' : ' ', i,
-		           tickless_verify_records[i].need,
-		           tickless_verify_records[i].hwtimer_diff,
-		           tickless_verify_records[i].ktimer_diff,
+		           i == verify_n - 1 ? '*' : ' ', i,
+		           verify_records[i].need,
+		           verify_records[i].hwtimer_diff,
+		           verify_records[i].ktimer_diff,
 		           hwtimer_need_diff >= 0 ? ' ' : '-',
 		           hwtimer_need_diff >= 0 ? hwtimer_need_diff : -hwtimer_need_diff,
 		           hwtimer_ktimer_diff >= 0 ? ' ' : '-',
 		           hwtimer_ktimer_diff >= 0 ? hwtimer_ktimer_diff : -hwtimer_ktimer_diff,
-		           tickless_verify_records[i].count,
-		           tickless_verify_records[i].count_int);
+		           verify_records[i].count,
+		           verify_records[i].count_int);
 #endif /* CONFIG_KDB */
 		sum += hwtimer_need_diff;
 	}
 
-	*times = tickless_verify_times;
+	*times = verify_times;
 
 	/* Tickless entering compensation */
-	for (i = 0; i < n; i++) {
-		if (tickless_verify_records[i].count_int == 0) {
-			int32_t diff = tickless_verify_records[i].hwtimer_diff - tickless_verify_records[i].ktimer_diff;
+	for (int i = 0; i < n; i++) {
+		if (verify_records[i].count_int == 0) {
+			int32_t diff = verify_records[i].hwtimer_diff - verify_records[i].ktimer_diff;
 
 			if (diff > 0) {
-				int new_compensation = diff / tickless_verify_records[i].count;
+				int new_comp = diff / verify_records[i].count;
 
-				if (tickless_verify_records[i].count * new_compensation < diff) {
-					new_compensation++;
+				if (verify_records[i].count * new_comp < diff) {
+					new_comp++;
 				}
 
-				if (new_compensation < compensation) {
-					compensation = new_compensation;
+				if (new_comp < compensation) {
+					compensation = new_comp;
 				}
 			}
 		}
 	}
 
 	/* Tickless interrupted compensation */
-	for (i = 0; i < n; i++) {
-		if (tickless_verify_records[i].count_int != 0) {
-			int32_t diff = tickless_verify_records[i].hwtimer_diff - tickless_verify_records[i].ktimer_diff
-			               - tickless_verify_records[i].count * compensation;
+	for (int i = 0; i < n; i++) {
+		if (verify_records[i].count_int == 0)
+			continue;
 
-			if (diff > 0) {
-				int new_int_compensation = diff / tickless_verify_records[i].count_int;
+		int32_t diff = verify_records[i].hwtimer_diff -
+		               verify_records[i].ktimer_diff -
+		               verify_records[i].count * compensation;
 
-				if (tickless_verify_records[i].count_int * new_int_compensation < diff) {
-					new_int_compensation++;
-				}
+		if (diff > 0) {
+			int new_int_comp = diff / verify_records[i].count_int;
 
-				if (new_int_compensation < int_compensation) {
-					int_compensation = new_int_compensation;
-				}
+			if (verify_records[i].count_int * new_int_comp < diff) {
+				new_int_comp++;
+			}
+
+			if (new_int_comp < int_compensation) {
+				int_compensation = new_int_comp;
 			}
 		}
 	}
